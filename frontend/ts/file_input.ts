76b053322096df15f520f4b9240fd72d6d35d30e
alexander-g <3867427+alexander-g@users.noreply.github.com>
2023-03-25 14:16:16 +0000

    loading result files [WIP]

--- frontend/ts/file_input.ts
+++ frontend/ts/file_input.ts
@@ -1,9 +1,9 @@
-import { preact, UTIF } from "./dep.ts"
+import { JSX, UTIF }    from "./dep.ts"
 import * as util        from "./util.ts"
 import { STATE }        from "./state.ts"   //TODO: hard-coded
 
 /** Event handler for file drag events */
-export function on_drag(event:preact.JSX.TargetedDragEvent<HTMLElement>): void {
+export function on_drag(event:JSX.TargetedDragEvent<HTMLElement>): void {
     event.preventDefault()
 }
 
@@ -16,15 +16,106 @@ export async function on_drop(event:preact.JSX.TargetedDragEvent<HTMLElement>): 
     const files: FileList | undefined = event.dataTransfer?.files
     //refresh ui
     await util.wait(1)
-    //not set the state with the actual files
-    STATE.files.set_from_files(files ?? [])  //TODO: hard-coded
+    //now set the state with the actual files
+    load_list_of_files_default(files ?? [])
 }
+
+
+/** Load files, some might be input files, others results 
+ *  @param file_list            - The list of files to load.
+ *  @param input_file_types     - File types that are interpreted as input files.
+ *  @param load_input_files_fn  - Callback that sets the input files.
+ *  @param load_result_files_fn - Callback that loads the result files.
+ */
+export function load_list_of_files(
+    file_list:                  FileList|File[],
+    input_file_types:           string[],
+    load_input_files_fn:        (files:File[]) => void,
+    load_result_files_fn:       (files:File[]) => void,
+): void {
+    const files: File[]         = Array.from(file_list)
+    const inputfiles: File[]    = files.filter(
+        (f:File) => input_file_types.includes(f.type)
+    )
+    if(inputfiles.length)
+        load_input_files_fn(inputfiles)
+    
+    const remaining_files: File[] = files.filter((f:File) => !inputfiles.includes(f))
+    load_result_files_fn(remaining_files)
+}
+
+
+/** Load input or result files @see {@link load_list_of_files}, uses global state */
+export function load_list_of_files_default(file_list:FileList|File[]): void {
+    //TODO: still too hard-coded
+    return load_list_of_files(
+        file_list, 
+        ["image/jpeg", "image/tiff"],               //NOTE: no png
+        (inputfiles:File[]) => globalThis.STATE.files.set_from_files(inputfiles),
+        (maybe_resultfiles:File[]) => load_result_files(
+            maybe_resultfiles, globalThis.STATE.files.peek()
+        )
+    )
+}
+
+
+/** Load files as results if the match already loaded input files */
+function load_result_files(maybe_resultfiles: FileList|File[], loaded_inputfiles:File[]){
+    const result_files:Record<string, File[]> 
+        = collect_result_files(Array.from(maybe_resultfiles), loaded_inputfiles)
+    console.warn('Settings result files not implemented:', result_files)
+}
+
+
+/** Filter a list of files, matching result files to already loaded input files 
+ * @param maybe_result_files - Array of File objects that may include result files.
+ * @param input_files        - Array of File objects representing the input files that have already been loaded.
+ * @returns Object mapping input file names to arrays of File objects representing the result files that match each input file.
+ */
+export function collect_result_files(
+    maybe_result_files: File[],
+    input_files:        File[],
+): Record<string, File[]> {
+    const collected: Record<string, File[]> = {}
+    for(const maybe_resultfile of maybe_result_files){
+        const zip_filetypes:string[] = ["application/zip", "application/x-zip-compressed"]
+        if(zip_filetypes.includes(maybe_resultfile.type)){
+            //TODO: implement
+            console.error('Loading zip files not yet implemented')
+            continue;
+        }
+        //else
+        /** Check for every input file if this result file matches */
+        for(const inputfile of input_files){
+            if(match_resultfile_to_inputfile(inputfile, maybe_resultfile)){
+                const prev: File[] = collected[inputfile.name] ?? []
+                collected[inputfile.name] = prev.concat([maybe_resultfile])
+            }
+        }
+    }
+    return collected;
+}
+
+/** Return true if the result file matches the input file */
+function match_resultfile_to_inputfile(inputfile:File, maybe_resultfile:File): boolean {
+    const basename: string         = util.file_basename(maybe_resultfile.name)
+    const no_ext_filename:string   = util.remove_file_extension(inputfile.name)
+    const candidate_names:string[] = [
+        inputfile.name  + '.json',
+        no_ext_filename + '.json',
+        inputfile.name  + '.result.json',
+        no_ext_filename + '.result.json'
+    ]
+    return (candidate_names.indexOf(basename) != -1)
+}
+
+
 
 export function set_image_src(img:HTMLImageElement, input:Blob|string|null): void {
     if( (input instanceof File)
      && (input.type=="image/tiff" || input.name.endsWith('.tif') || input.name.endsWith('.tiff'))) {
         load_tiff_file_as_blob(input).then( blob => set_image_src(img, blob) )
-    } else if( input instanceof Blob) {
+    } else if( input instanceof Blob) { //TODO: should check file type and give error if unsupported
         const url:string = URL.createObjectURL(input)
         img.style.visibility = '';
         img.addEventListener( 'load', () => URL.revokeObjectURL(url), {once:true} )
--- tests/testcases_deno/file_input.test.ts
+++ tests/testcases_deno/file_input.test.ts
@@ -1,5 +1,5 @@
-import * as file_input   from "../../frontend/ts/file_input.ts"
-import { asserts, path } from "./dep.ts"
+import * as file_input          from "../../frontend/ts/file_input.ts"
+import { asserts, path, mock }  from "./dep.ts"
 
 const IMAGE_ASSET1_PATH: string 
     = path.fromFileUrl(import.meta.resolve('../testcases/assets/test_image2.tiff'))
@@ -27,14 +27,57 @@ Deno.test("imagetools.load_tiff", async () => {
 })
 
 
-/* Deno.test('imagetools.load_tiff_to_blob', async () => {
-    const _document:Document = await util.setup_jsdom()
-    //util.mock_jQ( {accordion:mock.spy()} )
+//TODO: test that files are reset
+Deno.test('load_list_of_files', () => {
+    const mock_files: File[] = [
+        new File([""], "input.jpg",  { type: "image/jpeg" }),
+        new File([""], "result.zip", { type: "application/zip" }),
+        new File([""], "input.tiff", { type: "image/tiff" }),
+        new File([""], "jpg_without_jpg",  { type: "image/jpeg" }),
+    ];
 
-    const tiffdata: Uint8Array          = Deno.readFileSync(IMAGE_ASSET1_PATH)
-    const tifffile                      = new File([tiffdata], 'image1.tiff')
-    const decoded_blob:Blob|null        = await file_input.load_tiff_file_as_blob(tifffile)
-    asserts.assertExists(decoded_blob)
+    const set_inputfiles_mock: mock.Spy     = mock.spy()
+    const set_resultfiles_mock: mock.Spy    = mock.spy()
+
+    file_input.load_list_of_files(
+        mock_files,
+        ["image/jpeg", "image/tiff"],
+        set_inputfiles_mock,
+        set_resultfiles_mock,
+    );
+
+    mock.assertSpyCalls(set_inputfiles_mock,  1)
+    mock.assertSpyCallArg(set_inputfiles_mock, 0, 0, [mock_files[0], mock_files[2], mock_files[3]])
+
+    mock.assertSpyCalls(set_resultfiles_mock, 1)
+    mock.assertSpyCallArg(set_resultfiles_mock, 0, 0, [mock_files[1]])
 })
- */
 
+
+Deno.test("collect_result_files filters result files for input files", () => {
+    // Define some input and result files
+    const input_files: File[] = [
+        new File([], "input1.jpg"),
+        new File([], "input2.tiff"),
+        new File([], "input3.tiff"),
+    ];
+    const result_files: File[] = [
+        new File([], "input1.json"),
+        new File([], "input1.jpg.result.json"),
+        new File([], "input2.jpg"),
+        new File([], "input2.json"),
+        new File([], "input4.jpg"),
+    ];
+
+    // Call the function being tested
+    const collected: Record<string, File[]> 
+        = file_input.collect_result_files(result_files, input_files);
+
+    // Check that the function returns the correct output
+    asserts.assertEquals(collected['input1.jpg']?.length, 2);
+    asserts.assertEquals(collected['input1.jpg']?.[0], result_files[0]);
+    asserts.assertEquals(collected['input1.jpg']?.[1], result_files[1]);
+    asserts.assertEquals(collected['input2.tiff']?.length, 1);
+    asserts.assertEquals(collected['input2.tiff']?.[0]?.name, "input2.json");
+    asserts.assertEquals(collected['input3.tiff'], undefined);
+});
